8ab564340ee42a3254f18a3a91a1dc7af37cdbc7
Michael Matos <mikematos84@gmail.com>
2017-03-14 00:05:56 -0400

    Synced up branches

---
+++ api/v1/core/router.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+* Router
+* 
+* Takes in the request, parses it, and validates it against predefined routes found in the
+* "routes" folder. If a match is made, the requested endpoint's functionality is carried out
+* and any necessary variables are passed along the way.
+*
+* any digit = (\d+)
+* any word = (\w+)
+* any mixture of words, decimals, hyphens, and underscores = ([\w+\W+]*)
+*/
+
+class Router {
+	
+    private static $routes = array();
+    private function __construct() {}
+    private function __clone() {}
+
+    public static function init(){
+        require_once __DIR__ . '/db.php';
+        require_once __DIR__ . '/response.php';
+        self::loadDefinedRoutes();
+    }
+
+    public static function loadDefinedRoutes(){
+        $files = array_diff(scandir(__DIR__ . '../../routes'), ['.','..']);
+        foreach($files as $file){
+            require_once __DIR__ . '../../routes/' . $file;
+        }
+    }
+
+	public static function route($pattern, $callbacks = []) {
+        $pattern = '/^' . str_replace('/', '\/', $pattern) . '$/';
+
+        if(array_key_exists($pattern, self::$routes) == false){
+            self::$routes[$pattern] = [];
+        }
+
+        foreach($callbacks as $method => $callback){
+            self::$routes[$pattern][strtolower($method)] = $callback;
+        }
+    }
+
+    public static function execute($route) {
+        $request = strtolower($_SERVER['REQUEST_METHOD']);
+        $response = [];
+
+        foreach (self::$routes as $pattern => $methods) {
+			if (preg_match($pattern, $route, $params)) {
+				if(array_key_exists($request, $methods)){
+                    array_shift($params);
+                    return call_user_func_array($methods[$request], array_values($params));
+                };
+            }
+		}
+
+        self::error($request, $route, 404);
+	}
+
+    public static function error($request, $route, $status, $message = ''){
+        $response = [];
+        $response['status'] = $status;
+        $response['request'] = strtoupper($request);
+        $response['route'] = $route;
+
+        switch($status){
+            case 404:
+                $response['message'] = 'a "' . $request . '" request with route matching "' . $route . '" could not be found';
+            break; 
+        }
+
+        if($message){
+            $response['message'] = $message;
+        }
+
+        Response::json($response);
+    }
+}
---
+++ api/v1/routes/user.php
@@ -0,0 +1,65 @@
+<?php
+
+Router::route('user', [
+    'get' => function(){
+        $response = [];
+        Response::json($response);
+    }
+]);
+
+Router::route('user/(\d+)', [
+    'get' => function($id){
+        Database::query('SELECT * FROM users WHERE id=?', [$id], function($resp){
+            Response::json($resp);
+        });
+    },
+    'post' => function($id){
+        $db = Database::connect();
+        $stmt = $db->Prepare('INSERT INTO users (login_id, password) VALUES (?,?)');
+        $response = $db->Execute($stmt, ['mikematos84@gmail.com', 'open(this)']);
+        $response->insertID = $db->Insert_ID();
+        Response::json($response);
+        $db->Close();
+    },
+    'put' => function($id){
+        if(isset($id) == false){
+            Response::json(['error' => 'No ID was passed']);
+        }
+
+        $db = Database::connect();
+        $stmt = $db->Prepare('UPDATE users SET password=? WHERE id=?');
+        $response = $db->Execute($stmt, ['open(this)', $id]);
+        $response->insertID = $db->Insert_ID();
+        Response::json($response);
+        $db->Close();
+    },
+    'delete' => function($id){
+        if(isset($id) == false){
+            Response::json(['error' => 'No ID was passed']);
+        }
+
+        $db = Database::connect();
+        $stmt = $db->Prepare('DELETE FROM users WHERE id=?');
+        $response = $db->Execute($stmt, [$id]);
+        $response->insertID = $db->Insert_ID();
+        Response::json($response);
+        $db->Close();
+    }
+]);
+
+Router::route('user/(\w+)/(\d+)', [
+    'get' => function($action, $id){
+        $response = [];
+        $response['action'] = $action;
+        $response['id'] = $action;
+        Response::json($response);
+    }
+]);
+
+Router::route('user/([\w+\W+]*)', [
+    'get' => function($post){
+        $response = [];
+        $response['post'] = $post;
+        Response::json($response);
+    }
+]);
