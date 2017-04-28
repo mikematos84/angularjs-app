cebabc3e74ca80d56f7f9b0a9bc28ed9d0151699
Michael Matos <mikematos84@gmail.com>
2017-04-27 23:43:27 -0400

    run-sequence; rename;

--- gulpfile.js
+++ gulpfile.js
@@ -6,21 +6,24 @@ const sass = require('gulp-sass');
 const jshint = require('gulp-jshint');
 const imagemin = require('gulp-imagemin');
 const htmlmin = require('gulp-htmlmin');
+const rename = require('gulp-rename');
 
 const history = require('connect-history-api-fallback');
 const merge = require('merge-stream');
 const browserSync = require('browser-sync').create();
 const del = require('del');
+const runSequence = require('run-sequence');
 
 const scripts = require('./scripts');
 const styles = require('./styles');
+const src = './src';
 
 var devMode = false;
-var paths = {
-    get dest(){ return './dist'; },
+var dest = {
+    get dest(){ return devMode ? './dist' : './test'; },
     get css(){ return this.dest + '/css'; },
     get js(){ return this.dest + '/js'; },
-    get images(){ return this.dest + '/images'; }
+    get images(){ return this.dest + '/img'; }
 }
 
 gulp.task('vendor', function(){
@@ -28,67 +31,87 @@ gulp.task('vendor', function(){
         gulp.src(styles)
             .pipe(concat('vendor.css'))
             .pipe(sass({outputStyle: devMode === false ? 'compressed' : 'expanded'}).on('error', sass.logError))
-            .pipe(gulp.dest(paths.css)),
+            .pipe(gulp.dest(dest.css)),
         gulp.src(scripts)
             .pipe(concat('vendor.js'))
             .pipe(gulpif(devMode === false, uglify({mangle:false})))
-            .pipe(gulp.dest(paths.js))
+            .pipe(gulp.dest(dest.js))
     )
 });
 
 gulp.task('css', function(){
-    return gulp.src(["./src/scss/**/*.scss"])
+    return gulp.src([src + '/scss/**/*.scss'])
         .pipe(concat('main.css'))
         .pipe(sass({outputStyle: devMode === false ? 'compressed' : 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest(paths.css))
+        .pipe(gulp.dest(dest.css))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
 gulp.task('js', function(){
-    return gulp.src(["./src/**/*.js"])
+    return gulp.src([src + '/**/*.js'])
         .pipe(jshint({laxcomma: true}))
         .pipe(jshint.reporter('default'))
         .pipe(concat('main.js'))
         .pipe(gulpif(devMode === false, uglify({mangle:false})))
-        .pipe(gulp.dest(paths.js))
+        .pipe(gulp.dest(dest.js))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
 gulp.task('html', function(){ 
-    return gulp.src('./src/**/*.html')
-        .pipe(gulpif(devMode === false, htmlmin({collapseWhitespace: true})))
-        .pipe(gulp.dest(paths.dest))
+    return gulp.src(src + '/**/*.html')
+        // .pipe(gulpif(devMode === false, htmlmin({collapseWhitespace: true})))
+        .pipe(gulp.dest(dest.dest))
         .pipe(browserSync.reload({
             stream: true
         }));
 });
 
 gulp.task('images', function(){
-    return gulp.src('./src/images/*')
+    return gulp.src(src + '/img/**/*.+(png|jpg|gif|svg)')
         .pipe(gulpif(devMode === false, imagemin([
             imagemin.gifsicle({interlaced: true}),
             imagemin.jpegtran({progressive: true}),
             imagemin.optipng({optimizationLevel: 7}),
             imagemin.svgo({plugins: [{removeViewBox: true}]})
         ], {verbose: true})))
-        .pipe(gulp.dest(paths.images))
+        .pipe(gulp.dest(dest.images))
 })
 
 gulp.task('copy', function(){
-    return gulp.src(['./src/*.+(config|xml|png|ico)','./src/.*+(htaccess)'])
-    .pipe(gulp.dest(paths.dest));
+    return gulp.src([
+        src + '/*.+(config|xml|png|ico)',
+        src + '/.*+(htaccess)'
+    ])
+    .pipe(gulp.dest(dest.dest));
 })
 
 gulp.task('clean', function(){
-    return del(paths.dest);
+    return del(dest.dest);
+})
+
+gulp.task('watch', function(){
+    gulp.watch([src + '/**/*.+(css|scss)'], ['css']);
+    gulp.watch([src + '/**/*.js'], ['js']);
+    gulp.watch([src + '/**/*.html'], ['html']);
+    gulp.watch([src + '/img/**/*.+(png|jpg|gif|svg)'], ['images'])
+})
+
+gulp.task('browser-sync', function(){
+    return browserSync.init({
+        open: false,
+        server: {
+            baseDir: dest.dest,
+            middleware: [history()]
+        }
+    });
 })
 
 gulp.task('build', ['clean'], function(){
-    gulp.start(['vendor','css','js','html','images','copy','browser-sync','watch'], function(){
+    runSequence(['vendor','css','js','html','images','copy','watch'], function(){ 
         console.log(' -------------------------------------');
         console.log(' Build Type: ' + (devMode === true ? 'DEV' : 'PROD'));
         console.log(' -------------------------------------');
@@ -102,24 +125,9 @@ gulp.task('build', ['clean'], function(){
             console.log(' * Shorter load times');
         }
         console.log(' -------------------------------------');
-    })
-});
-
-gulp.task('watch', function(){
-    gulp.watch(['./src/**/*.+(css|scss)'], ['css']);
-    gulp.watch(['./src/**/*.js'], ['js']);
-    gulp.watch(['./src/**/*.html'], ['html']);
-})
-
-gulp.task('browser-sync', function(){
-    return browserSync.init(null, {
-        open: false,
-        server: {
-            baseDir: paths.dest,
-            middleware: [history()]
-        }
+        gulp.start('browser-sync');
     });
-})
+});
 
 gulp.task('default', function(){ 
     devMode = true;
