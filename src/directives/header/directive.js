bb6d3037dd53dc1db4a681594910a5eea976bad1
Michael Matos <mikematos84@gmail.com>
2017-05-09 17:26:02 -0400

    formatting

--- src/directives/header/directive.js
+++ src/directives/header/directive.js
@@ -1,15 +1,15 @@
 angular.module('app')
-.directive('header', function(){
-    return {
-        restrict: 'A',
-        replace: true,
-        templateUrl: './directives/header/index.html',
-        controller: 'HeaderController',
-        controllerAs: 'header'
-    };
-})
-.controller('HeaderController'
-,['$scope', 
-function($scope){
+    .directive('header', function () {
+        return {
+            restrict: 'A',
+            replace: true,
+            templateUrl: './directives/header/index.html',
+            controller: 'HeaderController',
+            controllerAs: 'header'
+        };
+    })
+    .controller('HeaderController'
+    , ['$scope',
+        function ($scope) {
 
-}]);
+        }]);
