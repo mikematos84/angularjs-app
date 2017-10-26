9b0a2e8ba864b3e64c9da695b3de5a60e9d306ba
Mike Matos <mikematos84@gmail.com>
2017-10-25 21:23:33 -0400

    Basic http example; gulp-json-minify

--- src/app/components/home/controller.js
+++ src/app/components/home/controller.js
@@ -1,13 +1,26 @@
 angular.module('app')
     .controller('HomeController', [
         '$scope',
+        '$stateParams',
+        '$http',
         HomeController]);
 
 function HomeController(
     $scope,
-    $stateParams
+    $stateParams,
+    $http
 ) {
     var self = this;
 
     $scope.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eu nunc vestibulum, malesuada mi quis, interdum tellus. Sed id risus elit. Sed eget viverra orci. In eget rhoncus tellus. Duis ac diam a nisl imperdiet egestas in et dolor. Praesent vitae lorem ut mi posuere blandit. Ut lacinia euismod felis ac efficitur. Phasellus vel nunc elit.";
+
+    /**
+     * Simple example of importing data from json. 
+     */
+    $http.get('data/test.json')
+        .then(function success(resp){ 
+            console.log(resp.data);
+        }, function error(resp){
+            console.log(resp);
+        });
 }
